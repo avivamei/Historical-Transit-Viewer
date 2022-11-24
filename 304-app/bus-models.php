bc196eb1cbe4a1d40d6605c581d1da53df938d2e
UBC Student <avivamei@gambier.students.cs.ubc.ca>
2022-11-23 21:18:28 -0800

    front end for bus models

--- 304-app/bus-models.php
+++ 304-app/bus-models.php
@@ -1,4 +1,25 @@
 <html>
 	<?php include 'secrets.php'; ?>
-	<h1>Placeholder</h1>
+	<head>
+        <title>CPSC 304 PHP/Oracle Demonstration</title>
+    </head>
+
+	<body>
+
+	<h1>Find the bus model of a specific bus</h1>
+
+	<h2>Insert Values into DemoTable</h2>
+        <form method="POST" action="bus-models.php"> <!--refresh page when submitted-->
+            <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
+            Number: <input type="text" name="insNo"> <br /><br />
+            Name: <input type="text" name="insName"> <br /><br />
+
+            <input type="submit" value="Insert" name="insertSubmit"></p>
+        </form>
+	<hr />
+
+	<?php
+	?>
+	</body>
+
 </html>
