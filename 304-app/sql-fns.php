dbee034ec6d28811ee9b43df1857dab5608e1145
Emily Chu <ubcechu@students.cs.ubc.ca>
2022-11-24 15:02:23 -0800

    dropdown population example

--- 304-app/sql-fns.php
+++ 304-app/sql-fns.php
@@ -1,5 +1,8 @@
 <?php
 include 'secrets.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 $success = True; //keep track of errors so it redirects the page only if there are no errors
 $db_conn = NULL; // edit the login credentials in connectToDB()
 $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
--- 304-app/tap-search.php
+++ 304-app/tap-search.php
@@ -2,14 +2,22 @@
     <?php include 'sql-fns.php'; ?>
     <h1>Tap search</h1>
     <form method="POST" action="oracle-test.php"> <!--refresh page when submitted-->
-        <select name="route" id="route" required="true">
-            <?
-             executeBoundSQL(select ())
-             ?>
+        <select name="route_name" id="route_name" required="true">
+        <?php
+         if (!connectToDB()) {
+         echo 'failed to connect to db';
+         return false;
+         }
+         $stid = executePlainSQL('SELECT route_number, route_name FROM CompassTap');
+         while (($row = oci_fetch_object($stid)) != false) {
+         echo var_dump($row);
+         ?>
+        <option value="<?= $row->ROUTE_NAME; ?>"><?= $row->ROUTE_NAME; ?></option>
+         <?php } ?>
         </select>
-        <input type="hidden" id="updateQueryRequest" name="updateQueryRequest">
-        Old Name: <input type="text" name="oldName"> <br /><br />
-        New Name: <input type="text" name="newName"> <br /><br />
+        <!-- <input type="hidden" id="updateQueryRequest" name="updateQueryRequest"> -->
+        <!-- Old Name: <input type="text" name="oldName"> <br /><br /> -->
+        <!-- New Name: <input type="text" name="newName"> <br /><br /> -->
         <input type="submit" value="Search" name="searchSubmit"></p>
 </form>
 <p></p>
