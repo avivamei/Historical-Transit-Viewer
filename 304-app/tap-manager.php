0051bf8cb0f03016a6aeff680f2d9dc0dcc7e17d
UBC Student <tdhenley@students.cs.ubc.ca>
2022-11-25 18:39:53 -0800

    added insert/edit, still need delete + display

--- 304-app/tap-manager.php
+++ 304-app/tap-manager.php
@@ -13,7 +13,81 @@ function onSubmit() {
     echo '<br><br><br>';
 }
 
+function handleInsertRequest() {
+		global $db_conn;
+
+		//Getting the values from user and insert data into the table
+		$tuple = array (
+				":bind1" => $_POST['card_id'],
+				":bind2" => $_POST['tap_date'],
+				":bind3" => $_POST['route_name'],
+				":bind4" => $_POST['stop_id'],
+		);
+
+		$alltuples = array (
+				$tuple
+		);
+
+		executeBoundSQL("insert into tap values (:bind1, :bind2, :bind3, :bind4)", $alltuples);
+		OCICommit($db_conn);
+}
+
+function handleUpdateRequest() {
+		global $db_conn;
+
+		$oldID = $_POST['oldID']
+		$newID = $_POST['newID']
+
+		$oldtime = $_POST['oldTime'];
+		$newtime = $_POST['newTime'];
+
+		$oldstop = $_POST['oldStop']
+		$newstop = $_POST['newStop']
+
+		executePlainSQL("UPDATE CompassTap SET card_id = " . $newid .", time =  " . $newtime .", stop = " . $newstop ." WHERE card_id = " . $oldid ." AND time = " . $oldtime ." AND stop = " . $oldstop);
+
+		OCICommit($db_conn);
+}
+
+function printResult($result, $header, $name) {
+    $size = count($header);
+
+    $includeHeader = true;
+
+    //table name
+    echo "<b>" . $name . "<b>";
+
+    echo "<table border='1'>";
+
+    //rows
+    while ($rows = OCI_Fetch_Array($result, OCI_BOTH)) {
+
+        // header
+        if ($includeHeader == true) {
+            $includeHeader = false;
+            echo "<tr>";
+
+            foreach($header as $value){
+                echo  "<th>" . $value . "</th>";
+            }
+            echo "</tr>";
+        }
+
+        echo "<tr>";
+
+        for($x = 0; $x < $size; $x++){
+            echo "<td>" . $rows[$x] . "</td>";
+        }
+
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
 ?>
+
+
+
 <html>
     <head>
         <title>Tap manager</title>
@@ -32,7 +106,7 @@ function onSubmit() {
                     echo 'failed to connect to db. Probably too many open connections.';
                     return false;
                 }
-                $stid = executePlainSQL('select distinct route_number, route_name from CompassTap');
+                $stid = executePlainSQL('select distinct route_number, route_name from Route1');
                 while (($row = oci_fetch_object($stid)) != false) {
                     echo var_dump($row);
                 ?>
@@ -41,10 +115,94 @@ function onSubmit() {
             </select>
             <select id="stop_id" name="stop_id">
                 <option value="*" selected>-- Select stop --</option>
-                <?php /*todo*/ ?>
+								<?php
+								if (!connectToDB()) {
+										echo 'failed to connect to db. Probably too many open connections.';
+										return false;
+								}
+								$stid = executePlainSQL('select distinct id, name from Stop');
+								while (($row = oci_fetch_object($stid)) != false) {
+										echo var_dump($row);
+								?>
+										<option value="<?= $row->ID." ".$row->NAME ?>"><?= $row->ID." ".$row->NAME; ?></option>
+								<?php } ?>
             </select>
             <input type="submit" value="Go!" name="submitName">
         </form>
+
+			<h3>Edit a compass tap</h3>
+				<form method="POST" action="tap-manager.php">
+
+					Old Card ID: <input type="text" name='oldid'> <br /><br />
+					New Card ID: <input type="text" name='newid'> <br /><br />
+
+					Old Date: <input type="date" name='olddate'> <br /><br />
+					New Date: <input type="date" name='newdate'> <br /><br />
+
+					<select name="oldroute" id="oldroute" required="true">
+							<option value="*" selected>-- Select old route --</option>
+							<?php
+							if (!connectToDB()) {
+									echo 'failed to connect to db. Probably too many open connections.';
+									return false;
+							}
+							$stid = executePlainSQL('select distinct route_number, route_name from Route1');
+							while (($row = oci_fetch_object($stid)) != false) {
+									echo var_dump($row);
+							?>
+									<option value="<?= $row->ROUTE_NUMBER." ".$row->ROUTE_NAME ?>"><?= $row->ROUTE_NUMBER." ".$row->ROUTE_NAME; ?></option>
+							<?php } ?>
+					</select>
+					<select name="newroute" id="newroute" required="true">
+							<option value="*" selected>-- Select new route --</option>
+							<?php
+							if (!connectToDB()) {
+									echo 'failed to connect to db. Probably too many open connections.';
+									return false;
+							}
+							$stid = executePlainSQL('select distinct route_number, route_name from Route1');
+							while (($row = oci_fetch_object($stid)) != false) {
+									echo var_dump($row);
+							?>
+									<option value="<?= $row->ROUTE_NUMBER." ".$row->ROUTE_NAME ?>"><?= $row->ROUTE_NUMBER." ".$row->ROUTE_NAME; ?></option>
+							<?php } ?>
+					</select>
+
+					<select id="oldstop" name="oldstop">
+							<option value="*" selected>-- Select old stop --</option>
+							<?php
+							if (!connectToDB()) {
+									echo 'failed to connect to db. Probably too many open connections.';
+									return false;
+							}
+							$stid = executePlainSQL('select distinct id, name from Stop');
+							while (($row = oci_fetch_object($stid)) != false) {
+									echo var_dump($row);
+							?>
+									<option value="<?= $row->ID." ".$row->NAME ?>"><?= $row->ID." ".$row->NAME; ?></option>
+							<?php } ?>
+					</select>
+					<select id="newstop" name="newstop">
+							<option value="*" selected>-- Select new stop --</option>
+							<?php
+							if (!connectToDB()) {
+									echo 'failed to connect to db. Probably too many open connections.';
+									return false;
+							}
+							$stid = executePlainSQL('select distinct id, name from Stop');
+							while (($row = oci_fetch_object($stid)) != false) {
+									echo var_dump($row);
+							?>
+									<option value="<?= $row->ID." ".$row->NAME ?>"><?= $row->ID." ".$row->NAME; ?></option>
+							<?php } ?>
+					</select>
+
+
+					<input type="hidden" id="updateRequest" name='updateRequest'>
+					<input type="submit" value ="Confirm and Update" name="updateTuples"></p>
+			</form>
+
+
 		<table>
 			<!-- todo render taps -->
 		</table>
