3f0dda247afd5bc4132201262903a7e6218c70e2
aviva <avivamei@gambier.students.cs.ubc.ca>
2022-11-26 20:31:08 -0800

    edited home page

--- 304-app/index.php
+++ 304-app/index.php
@@ -1,10 +1,8 @@
 <html>
 	<?php include 'secrets.php'; ?>
 	<h1>Welcome</h1>
-	<a href="./test-php.php"> <p>test php</p> </a>
-	<a href="./test-oracle.php"> <p>test oracle</p> </a>
-	<h1>actual project</h1>
-
+	<a href="./reset-tables.php"> <p>reset tables</p> </a>
+	
 	<br>INSERT/UPDATE/DELETE <br>
 	<a href="./tap-manager.php"> <p>tap manager</p> </a>
 
@@ -23,8 +21,5 @@
 	<a href="./having.php"> <p>bus models: max capacity with more than 1 model</p> </a>
 	<br>Nested Aggregation with Group By <br>
 	<a href="./nestedAggregation.php"> <p>bus models: max capacity larger than the average capacity of all bus models</p> </a>
-
-	<br><br><br>
-	<a href="./reset-tables.php"> <p>reset tables</p> </a>
 
 </html>
